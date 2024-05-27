0a22dd1cc23dd00686a621fa052e132517f6aed0
RocioOnsari <93567936+RocioOnsari@users.noreply.github.com>
2024-05-27 20:28:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -162,10 +162,7 @@ elif option == "Upload csv":
                                ["%y/%m/%d", "%d/%m/%y", '%m/%d/%y', "%y-%m-%d", "%d-%m-%y", '%m-%d-%y'])
 
         df.dropna(inplace=True)
-        time_selected = st.checkbox("Date")
-        if time_selected is Not None
-            df[date_column] = pd.to_datetime(df[date_column], format=format_date, origin="unix")
-        else: df[date_column] = df[date_column]
+
         st.write('You selected as q column:', q_column)
 
 elif option == "Upload excel":
